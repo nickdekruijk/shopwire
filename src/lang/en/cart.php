de1403e062db74e10fadc0df354732548742f8d0
Nick de Kruijk <git@nickdekruijk.nl>
2022-02-22 14:15:46 +0100

    Update cart.php

--- src/lang/en/cart.php
+++ src/lang/en/cart.php
@@ -5,7 +5,7 @@ return [
     'add_to_cart' => 'Add to cart',
     'update_cart' => 'Update cart',
     'checkout' => 'Checkout',
-    'empty' => 'Leeg',
+    'empty' => 'Empty',
     'decimal_point' => '.',
     'thousands_seperator' => ',',
 ];
