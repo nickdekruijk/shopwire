ce8ae5d6b455c1daee080b66799d3c174208b849
Nick de Kruijk <git@nickdekruijk.nl>
2022-02-28 20:13:24 +0100

    customerSorted attribute

--- src/Models/Order.php
+++ src/Models/Order.php
@@ -2,6 +2,7 @@
 
 namespace NickDeKruijk\Shopwire\Models;
 
+use Countries;
 use Illuminate\Database\Eloquent\Model;
 use NickDeKruijk\Shopwire\Shopwire;
 
@@ -45,6 +46,7 @@ class Order extends Model
         }
         return $value ?: $vat;
     }
+
     public function getAmountExclVatAttribute($value)
     {
         $vat = 0;
@@ -52,5 +54,35 @@ class Order extends Model
             $vat += $product['price']['price_excluding_vat'] * $product['quantity'];
         }
         return $value ?: $vat;
+    }
+
+    public function getCustomerSortedAttribute($value)
+    {
+        $values = [
+            'email' => $this->customer['email'],
+        ];
+        $group_status = [];
+
+        // First check if there are any groups
+        foreach (config('shopwire.checkout_form') as $column => $attributes) {
+            if (isset($attributes['toggle_group'])) {
+                $group_status[$attributes['toggle_group']] = $this->customer[$column] ?? false;
+            }
+        }
+
+        // Then loop over the customer data and add it to the array if the group is active
+        foreach (config('shopwire.checkout_form') as $column => $attributes) {
+            if (empty($attributes['group']) || $group_status[$attributes['group']]) {
+                if (str_contains($column, 'country') && isset($this->customer[$column]) && Countries::has($this->customer[$column], app()->getLocale())) {
+                    // Format country to current locale if it's a country
+                    $values[$column] = Countries::getOne($this->customer[$column], app()->getLocale());
+                } else {
+                    $values[$column] = $this->customer[$column] ?? '';
+                }
+            }
+        }
+
+        // Done, return it
+        return $values;
     }
 }
