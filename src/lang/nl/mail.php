b15c3f9d030aae81498046d9cc8a2ac04baabced
Nick de Kruijk <git@nickdekruijk.nl>
2022-02-28 20:06:04 +0100

    Create mail.php

---
+++ src/lang/nl/mail.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'salutation' => 'Beste',
+    'thank_you_for_your_order' => 'Bedankt voor de bestelling.',
+    'order_number' => 'Het bestelnummer is',
+    'we_have_received_your' => 'We hebben de',
+    'payment_and_the_following_order' => 'betaling ontvangen en de volgende bestelling:',
+    'your_order_will_be_shipped_to' => 'De producten worden verzonden naar:',
+    'question_about_your_order' => 'Bij vragen neem gerust contact op.',
+    'greeting' => 'Met vriendelijke groet,',
+    'sender_name' => config('shopwire.email_from.name'),
+
+    'new_order_from' => 'Nieuwe bestelling van ',
+];
