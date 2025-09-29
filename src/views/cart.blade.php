e446a1efa11c68fa6545cbb84450aeca035dec34
Nick de Kruijk <git@nickdekruijk.nl>
2025-09-29 14:01:38 +0200

    Remove trailing whitespace in cart view
    Cleaned up unnecessary trailing whitespace in the cart.blade.php file for improved code readability.

--- src/views/cart.blade.php
+++ src/views/cart.blade.php
@@ -5,6 +5,6 @@
     @if ($count)
         <span class="shopwire-cart-count">{{ $count }}</span>
     @else
-        <span class="shopwire-cart-empty">@lang('shopwire::cart.empty')</span>  
+        <span class="shopwire-cart-empty">@lang('shopwire::cart.empty')</span>
     @endif
 </a>
